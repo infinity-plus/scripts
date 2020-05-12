c338a1caf85eb06547d6891c8b0644c20b238097
infinity-plus <navdhareharsh2001@gmail.com>
2020-05-12 13:36:23 +0530

    changelog-generator.sh: Format according to shellcheck Fixing two warnings
    Signed-off-by: infinity-plus <navdhareharsh2001@gmail.com>

--- changelog-generator.sh
+++ changelog-generator.sh
@@ -6,9 +6,9 @@ changelog() {
 	UNTIL="$NEXT"
 	echo "CHANGELOG"
 	echo ----------------------
-	git log --no-merges --since="$SINCE" --until="$UNTIL" --format="%cd" --date=short | sort -u -r | while read DATE; do
+	git log --no-merges --since="$SINCE" --until="$UNTIL" --format="%cd" --date=short | sort -u -r | while read -r DATE; do
 		echo
 		echo ["$DATE"]
-		GIT_PAGER=cat git log --no-merges --format=" * %s" --since="$DATE 00:00:00" --until="$DATE 24:00:00"
+		git log --no-merges --format=" * %s" --since="$DATE 00:00:00" --until="$DATE 24:00:00"
 	done
 }
